e0b980ba1e54b71f6183415e3aa2f9ddd01374a5
mubkhh221 <mubkhh221@studentmail.oru.se>
2026-07-07 14:28:03 +0200

    Part 2: Add SQL compiler and virtual machine

--- SQL.c
+++ SQL.c
@@ -3,62 +3,135 @@
 #include <stdlib.h>
 #include <string.h>
 
-// created a struct to hold the state of our input buffer.
+// Part 1 Code
 typedef struct {
   char* buffer;
   size_t buffer_length;
   ssize_t input_length;
 } InputBuffer;
 
-// Memory allocation: malloc to allocate memory on the heap.
 InputBuffer* new_input_buffer() {
   InputBuffer* input_buffer = malloc(sizeof(InputBuffer));
   input_buffer->buffer = NULL;
   input_buffer->buffer_length = 0;
   input_buffer->input_length = 0;
-
   return input_buffer;
 }
 
 void print_prompt() { printf("db > "); }
 
-
-// It dynamically allocates memory for the buffer if it's NULL.
 void read_input(InputBuffer* input_buffer) {
   ssize_t bytes_read =
       getline(&(input_buffer->buffer), &(input_buffer->buffer_length), stdin);
-
   if (bytes_read <= 0) {
     printf("Error reading input\n");
     exit(EXIT_FAILURE);
   }
-
-  // Ignore trailing newline character that getline includes
   input_buffer->input_length = bytes_read - 1;
   input_buffer->buffer[bytes_read - 1] = 0;
 }
 
-// Memory Deallocation: because we used malloc we must use free.
-// If we don't, we create a memory leak, which will crash the DB later.
 void close_input_buffer(InputBuffer* input_buffer) {
-    free(input_buffer->buffer); // Free the buffer allocated by getline
-    free(input_buffer);         // Free the struct allocated by new_input_buffer
+    free(input_buffer->buffer);
+    free(input_buffer);
+}
+
+// Part 2 Code
+
+// ENUMS for error handling. 
+// Instead of returning true/false we return Enums. 
+// This makes the code selfdocumenting and forces the compiler 
+// to check if we handled every possible outcome in our switch statements.
+typedef enum {
+  META_COMMAND_SUCCESS,
+  META_COMMAND_UNRECOGNIZED_COMMAND
+} MetaCommandResult;
+
+typedef enum { 
+  PREPARE_SUCCESS, 
+  PREPARE_UNRECOGNIZED_STATEMENT 
+} PrepareResult;
+
+typedef enum { 
+  STATEMENT_INSERT, 
+  STATEMENT_SELECT 
+} StatementType;
+
+// The internal representation: This struct is our bytecode. 
+// Later, it will hold the actual data to insert, but for now it just holds the type of query.
+typedef struct {
+  StatementType type;
+} Statement;
+
+// Meta command handler: Checks for commands starting with a dot.
+MetaCommandResult do_meta_command(InputBuffer* input_buffer) {
+  if (strcmp(input_buffer->buffer, ".exit") == 0) {
+    close_input_buffer(input_buffer);
+    exit(EXIT_SUCCESS);
+  } else {
+    return META_COMMAND_UNRECOGNIZED_COMMAND;
+  }
+}
+
+// SQL compiler (Front-End): Converts text into our internal Statement struct.
+PrepareResult prepare_statement(InputBuffer* input_buffer, Statement* statement) {
+  // Notice we use strncmp compare first 6 chars instead of strcmp. 
+  // This is because later the user will type for example "insert 1 john john@email.com". 
+  // We only care about the first word for now!
+  if (strncmp(input_buffer->buffer, "insert", 6) == 0) {
+    statement->type = STATEMENT_INSERT;
+    return PREPARE_SUCCESS;
+  }
+  if (strcmp(input_buffer->buffer, "select") == 0) {
+    statement->type = STATEMENT_SELECT;
+    return PREPARE_SUCCESS;
+  }
+
+  return PREPARE_UNRECOGNIZED_STATEMENT;
+}
+
+// the virtual machine (Back-End): Executes the internal Statement.
+void execute_statement(Statement* statement) {
+  switch (statement->type) {
+    case (STATEMENT_INSERT):
+      printf("This is where we would do an insert.\n");
+      break;
+    case (STATEMENT_SELECT):
+      printf("This is where we would do a select.\n");
+      break;
+  }
 }
 
 int main(int argc, char* argv[]) {
   InputBuffer* input_buffer = new_input_buffer();
   
-  // The infinite loop of the REPL
   while (true) {
     print_prompt();
     read_input(input_buffer);
 
-    // Check if the user wants to exit
-    if (strcmp(input_buffer->buffer, ".exit") == 0) {
-      close_input_buffer(input_buffer);
-      exit(EXIT_SUCCESS);
-    } else {
-      printf("Unrecognized command '%s'.\n", input_buffer->buffer);
+    // STEP 1: Check if it's a Meta Command (starts with '.')
+    if (input_buffer->buffer[0] == '.') {
+      switch (do_meta_command(input_buffer)) {
+        case (META_COMMAND_SUCCESS):
+          continue;
+        case (META_COMMAND_UNRECOGNIZED_COMMAND):
+          printf("Unrecognized command '%s'\n", input_buffer->buffer);
+          continue;
+      }
     }
+
+    // STEP 2: Compile the SQL into a Statement (Front-End)
+    Statement statement;
+    switch (prepare_statement(input_buffer, &statement)) {
+      case (PREPARE_SUCCESS):
+        break;
+      case (PREPARE_UNRECOGNIZED_STATEMENT):
+        printf("Unrecognized keyword at start of '%s'.\n", input_buffer->buffer);
+        continue;
+    }
+
+    // STEP 3: Execute the Statement (Back-End)
+    execute_statement(&statement);
+    printf("Executed.\n");
   }
 }
